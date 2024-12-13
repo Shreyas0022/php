7e57a203b9b8f99a71a320e826d30164ba7d681d
Shreyas0022 <24mp2294@rit.ac.in>
2024-12-13 12:47:22 +0530

    Update webcycle4

---
+++ webcycle4/stud.php
@@ -0,0 +1,9 @@
+
+<?php
+
+$id=$_POST['id'];
+$name=$_POST['name'];
+$age=$_POST['age'];
+$class=$_POST['class'];
+echo "Name of Student:  $name<br><br> Id:  $id <br><br>Age:  $age <br><br> Department:  $class";
+?>
